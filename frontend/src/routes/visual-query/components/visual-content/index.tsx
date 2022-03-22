93f04a5e7e6e835f7f266c03a93ff2503f6c1217
hujian05 <hujian05@MacintoshdeMacBook-Pro.local>
2022-03-22 11:28:19 +0800

    [fix] add visual-query license

--- frontend/src/routes/visual-query/components/visual-content/index.tsx
+++ frontend/src/routes/visual-query/components/visual-content/index.tsx
@@ -1,12 +1,27 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 import React, { useContext, useImperativeHandle, useMemo, useRef } from 'react';
 import * as _ from 'lodash-es';
 import classnames from 'classnames';
-import { Button, Typography, Row, Spin } from 'antd';
+import { Typography, Row, Spin } from 'antd';
 import styles from './style.module.less';
 import VisualEditor from './visual-editor';
-import VisualTable from './visual-table';
 import { DatabasesContext, DataContext } from '../../context';
-import { ChartTypeEnum, SidebarTypeEnum } from '../../types';
 import LoadingLayout from '../loading-layout';
 
 function VisualContent(props: any, ref: any) {
